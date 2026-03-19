cfef37046c172cfd4c53515e7199bbf9c45f4e69
Almaz Fedorov <fedoro3valmaz@gmail.com>
2026-03-20 00:44:13 +0300

    Dynamic metrics and infinite loop

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,26 +4,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"runtime"
+	"time"
 )
 
 type Metrics struct {
-	OS   string `json:"os"`
-	CPUs int    `json:"cpus"`
+	OS       string `json:"os"`
+	CPUs     int    `json:"cpus"`
+	AllocRAM uint64 `json:"alloc_ram"`
 }
 
 func collectMetrics() Metrics {
 
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
 	return Metrics{
-		OS:   runtime.GOOS,
-		CPUs: runtime.NumCPU(),
+		OS:       runtime.GOOS,
+		CPUs:     runtime.NumCPU(),
+		AllocRAM: m.Alloc,
 	}
 }
 
 func main() {
-	jsonData, err := json.MarshalIndent(collectMetrics(), "", "  ")
-	if err != nil {
-		fmt.Println("Error marshalling to json")
-		return
+	for {
+		jsonData, err := json.MarshalIndent(collectMetrics(), "", "  ")
+		if err != nil {
+			fmt.Println("Error marshalling to json")
+			return
+		}
+		fmt.Println(string(jsonData))
+		time.Sleep(2 * time.Second)
 	}
-	fmt.Println(string(jsonData))
 }
